ce5f914d20bdbb37f1c249ad8d46b2c98f0e0ffc
alex <rojkov@gmail.com>
2015-08-20 10:42:43 -0700

    add web-session example

---
+++ examples/web-session/README.md
@@ -0,0 +1,15 @@
+Web Session demonstrates how to use Baratine JavaScript library to have a client
+browser receive events generated on the server side. 
+
+Note, that in this example communication takes place over websocket. 
+
+To run the example install maven-baratine-collection from 
+<https://github.com/baratine/maven-collection-baratine>
+
+Once the maven-collection-baratine is installed execute the command below from 
+examples/web-session directory.
+
+
+
+    mvn clean package baratine:run
+    
---
+++ examples/web-session/src/main/java/example/WebSession.java
@@ -0,0 +1,36 @@
+package example;
+
+import io.baratine.core.CancelHandle;
+import io.baratine.core.OnInit;
+import io.baratine.core.Result;
+import io.baratine.core.Service;
+import io.baratine.core.ServiceManager;
+import io.baratine.timer.TimerService;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Service("session://pod/session")
+public class WebSession
+{
+  private TimerService _timerService;
+
+  @OnInit
+  public void init()
+  {
+    _timerService
+      = ServiceManager.getCurrent().lookup("timer:///").as(TimerService.class);
+  }
+
+  public void registerListener(@Service EventListener listener)
+  {
+    System.out.println("WebSession.registerListener " + listener);
+
+    listener.onEvent(new Date().toString());
+    _timerService.runEvery(c -> listener.onEvent(new Date().toString()),
+                           1,
+                           TimeUnit.SECONDS,
+                           Result.<CancelHandle>ignore());
+  }
+}
+
